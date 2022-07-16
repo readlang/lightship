f2a8473bc4b89e24bab28b6c1c33cde9e988c9c7
Read Langworthy <rlangworthy@gmail.com>
2022-07-15 17:43:10 -0700

    update message serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,10 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :group_id, :text
+  attributes :id, :user_id, :group_id, :text, :user
+
+  # belongs_to :user
+
+  def user
+    object.user.username
+  end
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :profile_image, :city, :state, :country
-  # :password_digest, 
+  # removed :password_digest
 
 end
