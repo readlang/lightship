fe85691d80d66198cae188ed43f0d080e354ffa5
Read Langworthy <rlangworthy@gmail.com>
2022-07-19 21:01:07 -0700

    added redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+import store from './store';
+import {Provider} from 'react-redux'
 
 import 'bootstrap/dist/css/bootstrap.min.css';  // this is the bootstrap "style" file
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
