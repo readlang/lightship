adbffd92e273d25eb30ccabb4bd10179c3977a97
Read Langworthy <rlangworthy@gmail.com>
2022-09-27 15:34:16 -0700

    deleted "counterSlice" file

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import counterReducer from './slices/counterSlice'
 import userReducer from './slices/userSlice'
 import tracksReducer from './slices/tracksSlice'
 import actionsReducer from './slices/actionsSlice'
@@ -14,7 +13,6 @@ import errorsReducer from "./slices/errorsSlice"
 
 export default configureStore({
     reducer: {
-        counter: counterReducer, 
         user: userReducer,
         tracks: tracksReducer, 
         actions: actionsReducer,
